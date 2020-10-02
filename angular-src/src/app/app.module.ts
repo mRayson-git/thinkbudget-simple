e6507bf4761a335bb2dd9e882ba7d7e4fce72eaf
Michael Rayson <mrayson5129@gmail.com>
2020-10-02 08:27:23 -0400

    added angular with sass

---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+//microsoft graph api app id
+//460e97f5-a1a9-4fb9-a437-8020c65e8fad
